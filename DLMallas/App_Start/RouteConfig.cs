b2d798ac8580624588ac44f9b2956f4e69424241
jim <jim2008.cu@gmail.com>
2018-08-27 16:28:19 -0400

    - poniendo el controlador malla como controlador de inicio - comentando la linae Layout = "~/Views/Malla/Index.cshtml" en la vista _ViewStart.cshtml

--- DLMallas/App_Start/RouteConfig.cs
+++ DLMallas/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DLMallas
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Malla", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
