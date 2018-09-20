4a929454a5d99478e753c3962d841c5c0c5af579
jaigle <jaigle@gmail.com>
2018-09-20 09:50:36 -0300

    Inclusion de Dto de Itinerario

---
+++ DLMallas_Business/Dto/Itinerario/DtoItinerario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMallas.Business.Dto.Itinerario
+{
+    public class DtoItinerario
+    {
+        public string Nombre { get; set; }
+        public string Malla { get; set; }
+        public string Vigencia { get; set; }
+        public string Inscriptos { get; set; }
+        public string Estado { get; set; }
+        public string AvanUC { get; set; }
+        public string AvanColab { get; set; }
+    }
+
+    public class DtoNotificacionItinerario
+    {
+        public string Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public int Seleccionado { get; set; }
+    }
+
+}
