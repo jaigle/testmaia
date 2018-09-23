b50dd697e70e23741e05a5b392c649f5109536c5
jim <jim2008.cu@gmail.com>
2018-09-23 11:13:03 -0400

    94-Agregar Itinerario (UI)

--- DLMallas/Scripts/core/adminItinerario.js
+++ DLMallas/Scripts/core/adminItinerario.js
@@ -17,7 +17,7 @@
     // =========================================================================
 
     p.initialize = function (self) {
-        $('#tblItinerarios').dataTable({
+        $("#tblItinerarios").dataTable({
             responsive: true,
             orderCellsTop: true,
             fixedHeader: true,
@@ -26,44 +26,56 @@
                 "url": "/Content/DataTables/plugins/spanish.js"
             }
         });
-        var interval = setInterval(function () {
-            var html = '<div class="pull-right col-lg-12 col-md-12 col-sm-12">' +
-                        '<label class="pull-left col-lg-6 col-md-6 col-sm-6"><input type="text" placeholder="Buscar Itinerario" class="form-control input-sm" id="bItinerario"></label>' +
-                        '<label class="pull-right col-lg-6 col-md-6 col-sm-6"><input type="text" placeholder="Buscar Malla" class="form-control input-sm" id="bMalla"></label></div>';
 
-            $("#tblItinerarios_filter").html(html);
-            $("#tblItinerarios_length").addClass("col-lg-4 col-md-4 col-sm-4");
+        $('.date').datepicker({
+            format: "yyyy/mm/dd",
+            weekStart: 1,
+            //startDate: "now",
+            language: "es",
+            autoclose: true
+        });
 
-            $('#bItinerario, #bMalla').keyup(function () {
-                var table =  $('#tblItinerarios').DataTable();
-                table.draw();
-            });
-
-            window.clearInterval(interval);
-        }, 50);
-
-
-        $.fn.dataTable.ext.search.push(
-           function (settings, data, dataIndex) {
-               var itinerario = $('#bItinerario').val();
-               var malla = $('#bMalla').val();
-
-               var col1 =  data[1];
-               var col2 = data[2];
-               if ((itinerario === undefined && malla === undefined) || 
-                   (itinerario.length === 0 && malla.length === 0) ||
-                   (itinerario !== undefined && itinerario.length > 0 && col1.includes(itinerario)) || 
-                   (malla !== undefined && malla.length > 0 && col2.includes(malla)) ) {
-                   return true;
-               }
-               return false;
-           }
-       );
+        window.interval = setInterval(self.configuracion_vista, 50);
+        $.fn.dataTable.ext.search.push(self.dataTable_ext_search);
+        $("#btnCrearItinerario").click(self.crear_itinerario_click);
     };
 
     // Funciones privadas  ======================================================
 
+    p.configuracion_vista = function() {
+        var html = '<div class="pull-right col-lg-12 col-md-12 col-sm-12">' +
+            '<label class="pull-left col-lg-6 col-md-6 col-sm-6"><input type="text" placeholder="Buscar Itinerario" class="form-control input-sm" id="bItinerario"></label>' +
+            '<label class="pull-right col-lg-6 col-md-6 col-sm-6"><input type="text" placeholder="Buscar Malla" class="form-control input-sm" id="bMalla"></label></div>';
 
+        $("#tblItinerarios_filter").html(html);
+        $("#tblItinerarios_length").addClass("col-lg-4 col-md-4 col-sm-4");
+
+        $('#bItinerario, #bMalla').keyup(function() {
+            var table = $('#tblItinerarios').DataTable();
+            table.draw();
+        });
+
+        window.clearInterval(window.interval);
+    };
+
+    p.dataTable_ext_search = function (settings, data, dataIndex) {
+        var itinerario = $('#bItinerario').val();
+        var malla = $('#bMalla').val();
+
+        var col1 =  data[1];
+        var col2 = data[2];
+        if ((itinerario === undefined && malla === undefined) || 
+            (itinerario.length === 0 && malla.length === 0) ||
+            (itinerario !== undefined && itinerario.length > 0 && col1.includes(itinerario)) || 
+            (malla !== undefined && malla.length > 0 && col2.includes(malla)) ) {
+            return true;
+        }
+        return false;
+    }
+
+    p.crear_itinerario_click = function() {
+        $('#modCrearItinerario').modal('show');
+    }
 
     // Eventos =================================================================
 
--- DLMallas_Business/Extencions/ObtenerListadoItinerarioMallaExtention.cs
+++ DLMallas_Business/Extencions/ObtenerListadoItinerarioMallaExtention.cs
@@ -10,19 +10,21 @@ namespace DLMallas.Business.Extencions
 {
     static public class ObtenerListadoItinerarioMalla
     {
-        public static DtoItinerario Faker(this DtoItinerario item, string idMalla)
+        public static DtoItinerario Faker(this DtoItinerario item, int id, string idMalla)
         {
             return new Faker<DtoItinerario>("es")
                 .StrictMode(true)
+                .RuleFor(r => r.Id, id.ToString())
                 .RuleFor(r => r.Nombre, f => f.Name.JobArea())
                 .RuleFor(r => r.Malla, f => (idMalla != "0") ? idMalla.ToString() : f.Name.Random.Word())
                 .RuleFor(r => r.Vigencia,
                     f => f.Date.Past(1, null).ToString("dd/MM/yyyy") + " - " +
                          f.Date.Past(0, null).ToString("dd/MM/yyyy"))
                 .RuleFor(r => r.Inscriptos, f => f.Random.Number(1, 50).ToString())
-                .RuleFor(r => r.Estado, f => f.PickRandom(0,1).ToString())
+                .RuleFor(r => r.Estado, f => f.PickRandom(0, 1).ToString())
                 .RuleFor(r => r.AvanUC, f => f.Random.Number(0, 100).ToString() + "%")
-                .RuleFor(r => r.AvanColab, f => f.Random.Number(0, 100).ToString() + "%");
+                .RuleFor(r => r.AvanColab, f => f.Random.Number(0, 100).ToString() + "%")
+                .RuleFor(r => r.IdSociedad, f => f.Random.Number(0, 100).ToString());
         }
 
         public static List<DtoItinerario> Faker(this List<DtoItinerario> list, string idMalla)
@@ -30,7 +32,7 @@ namespace DLMallas.Business.Extencions
             for (
                 int i = 0; i < 100; i++)
             {
-                list.Add(new DtoItinerario().Faker((idMalla == "0") ? "0" : idMalla));
+                list.Add(new DtoItinerario().Faker(i+1, (idMalla == "0") ? "0" : idMalla));
             }
 
             return list;
