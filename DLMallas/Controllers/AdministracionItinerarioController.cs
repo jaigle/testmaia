ab1599716b88969e33d8284353b0d2f44b51718f
jim <jim2008.cu@gmail.com>
2018-09-23 16:21:06 -0400

    93-Agregar Itinerario	(Capa Logica)

--- DLMallas/Controllers/AdministracionItinerarioController.cs
+++ DLMallas/Controllers/AdministracionItinerarioController.cs
@@ -1,6 +1,9 @@
 using System.Collections.Generic;
+using System.Net;
 using System.Web.Mvc;
+using DLMallas.Business.Dto.Itinerario;
 using DLMallas.Models;
+using DLMallas.Utilidades;
 
 namespace DLMallas.Controllers
 {
@@ -17,6 +20,16 @@ namespace DLMallas.Controllers
 
             return View(model);
         }
+
+        public HttpStatusCodeResult GuardarItinerario(string mallaId, string nombre, string fechaInic, string fechaFin)
+        {
+            var resp = _Itinerario.GuardarItinerario(mallaId, nombre, fechaInic, fechaFin);
+            if (resp)
+                return new HttpStatusCodeResult(HttpStatusCode.OK, "Ok");
+
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Error intentando guardar malla");
+        }
+        
 
         public PartialViewResult  TablaSelecionarMalla()
         {
